2f425e4813bcda73085fa617f3cef0a05ded4628
Travis CI User <travis@example.org>
2017-04-03 11:51:35 +0000

    [ci skip] update gh-pages

--- build/app/assets.utility2.rollup.js
+++ build/app/assets.utility2.rollup.js
@@ -603,8 +603,12 @@ local.templateApidocMd = '\
             );
             local.objectSetDefault(options, {
                 env: {},
-                packageJson: JSON.parse(readExample('package.json'))
+                packageJson: {}
             });
+            try {
+                options.packageJson = JSON.parse(readExample('package.json'));
+            } catch (ignore) {
+            }
             Object.keys(options.packageJson).forEach(function (key) {
                 if (key[0] === '_') {
                     delete options.packageJson[key];
@@ -624,15 +628,18 @@ local.templateApidocMd = '\
                 template: local.templateApidocHtml
             });
             // init exampleList
-            options.exampleList = options.exampleList.concat(options.exampleFileList.concat(
-                local.fs.readdirSync(options.dir)
-                    .sort()
-                    .filter(function (file) {
-                        return file.indexOf(options.env.npm_package_main) === 0 ||
-                            (/^(?:readme)\b/i).test(file) ||
-                            (/^(?:index|lib|test)\b.*\.js$/i).test(file);
-                    })
-            ).map(readExample));
+            try {
+                options.exampleList = options.exampleList.concat(options.exampleFileList.concat(
+                    local.fs.readdirSync(options.dir)
+                        .sort()
+                        .filter(function (file) {
+                            return file.indexOf(options.env.npm_package_main) === 0 ||
+                                (/^(?:readme)\b/i).test(file) ||
+                                (/^(?:index|lib|test)\b.*\.js$/i).test(file);
+                        })
+                ).map(readExample));
+            } catch (ignore) {
+            }
             // init moduleMain
             try {
                 moduleMain = {};
@@ -11947,8 +11954,21 @@ return Utf8ArrayToStr(bff);
         /*
          * this function will build the npmdoc
          */
-            var onParallel, packageJson;
-            onParallel = local.utility2.onParallel(onError);
+            var done, onError2, onParallel, packageJson;
+            // ensure exit after 5 minutes
+            setTimeout(process.exit, 5 * 60 * 1000);
+            onError2 = function (error) {
+                local.onErrorDefault(error);
+                if (done) {
+                    return;
+                }
+                done = true;
+                // try to recover from error
+                setTimeout(onError, error && local.timeoutDefault);
+            };
+            // try to salvage uncaughtException
+            process.on('uncaughtException', onError2);
+            onParallel = local.utility2.onParallel(onError2);
             onParallel.counter += 1;
             // build package.json
             packageJson = JSON.parse(local.fs.readFileSync('package.json', 'utf8'));
